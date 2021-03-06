e3eb1f0a6e31ed0e1eb7c8c4fbc613fd814e5eeb
mouftaho <mouftaho@mcmaster.ca>
2021-03-06 13:33:12 -0500

    please work

--- healthe/lib/main.dart
+++ healthe/lib/main.dart
@@ -10,18 +10,34 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Login'),
+      routes: {
+        "/page1": (_) => new FirstRoute(),
+      },
+    );
+  }
+}
+
+
+//After Login Page
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Welcome Omar'),
+        automaticallyImplyLeading: true, //true shows button, false hides it
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Open route'),
+          onPressed: () {
+            // Navigate to second route when tapped.
+          },
+        ),
+      ),
     );
   }
 }
@@ -83,7 +99,9 @@ class _MyHomePageState extends State<MyHomePage> {
       child: MaterialButton(
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () {},
+        onPressed: () {
+          Navigator.pushNamed(context,"/page1");
+        },
         child: Text("Login",
             textAlign: TextAlign.center,
             style: style.copyWith(
