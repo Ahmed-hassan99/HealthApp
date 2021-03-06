23ed58b5b95a21c7097f53054bfbe2de979b8259
Ahmed Hassan <Ahmed99dodo1999@gmail.com>
2021-03-06 13:50:44 -0500

    Tabs pages added

--- healthe/lib/main.dart
+++ healthe/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import './FirstPage.dart' as first;
+import './SecondPage.dart' as second;
+import './ThirdPage.dart' as third;
 
 void main() => runApp(MyApp());
 
@@ -14,7 +17,7 @@ class MyApp extends StatelessWidget {
       ),
       home: MyHomePage(title: 'Flutter Login'),
       routes: {
-        "/page1": (_) => new FirstRoute(),
+        "/page1": (_) => new MyTabs(),
       },
     );
   }
@@ -22,25 +25,65 @@ class MyApp extends StatelessWidget {
 
 
 //After Login Page
-class FirstRoute extends StatelessWidget {
+
+class MyTabs extends StatefulWidget{
   @override
-  Widget build(BuildContext context) {
+  MyTabsState createState() => new MyTabsState();
+}
+
+class MyTabsState extends State<MyTabs> with SingleTickerProviderStateMixin{
+  
+  TabController controller;
+
+  @override
+  void initState(){
+    super.initState();
+    controller = new TabController(vsync: this, length: 3);
+  }
+
+  @override
+  void dispose(){
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context){
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Welcome Omar'),
-        automaticallyImplyLeading: true, //true shows button, false hides it
+      appBar: new AppBar(title: new Text("Pages"), backgroundColor: Colors.deepOrange,
+      bottom: new TabBar(
+          controller: controller,
+          tabs: <Tab>[
+            new Tab(icon: new Icon(Icons.arrow_forward)),
+            new Tab(icon: new Icon(Icons.arrow_downward)),
+            new Tab(icon: new Icon(Icons.arrow_back))
+          ]
+        )
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('Open route'),
-          onPressed: () {
-            // Navigate to second route when tapped.
-          },
-        ),
+      bottomNavigationBar: new Material(
+        color: Colors.deepOrange,
+        child: new TabBar(
+          controller: controller,
+          tabs: <Tab>[
+            new Tab(icon: new Icon(Icons.arrow_forward)),
+            new Tab(icon: new Icon(Icons.arrow_downward)),
+            new Tab(icon: new Icon(Icons.arrow_back))
+          ]
+        )
       ),
+      body: new TabBarView(
+        controller: controller,
+        children: <Widget>[
+          new first.First(),
+          new second.Second(),
+          new third.Third()
+        ]
+      )
     );
   }
 }
+
+
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
