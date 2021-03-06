a8a23110a33ae26cdceaf3c5a65430fc5ed9ff49
mouftaho <mouftaho@mcmaster.ca>
2021-03-06 16:04:52 -0500

    Implemented Dynamic updating to table

--- healthe/lib/FirstPage.dart
+++ healthe/lib/FirstPage.dart
@@ -1,61 +1,36 @@
 import 'package:flutter/material.dart';
 
 class First extends StatelessWidget {
-
+  final List<Map<String, String>> listOfColumns = [
+    {"Date": "AAAAAA", "Systolic": "1", "Diastolic": "Yes","Pulse": "90"},
+    {"Date": "BBBBBB", "Systolic": "2", "Diastolic": "no","Pulse": "90"},
+    {"Date": "CCCCCC", "Systolic": "3", "Diastolic": "Yes","Pulse": "90"},
+    {"Date": "CCCCCC", "Systolic": "3", "Diastolic": "Yes","Pulse": "90"},
+     {"Date": "CCCCCC", "Systolic": "3", "Diastolic": "Yes","Pulse": "90"}
+  ];
+//  DataTableWidget(this.listOfColumns);     // Getting the data from outside, on initialization
   @override
-Widget build(BuildContext context) {
-    return Table(
-      border: TableBorder.all(),
-      columnWidths: {
-        0: IntrinsicColumnWidth(),
-        1: IntrinsicColumnWidth(),
-        2: IntrinsicColumnWidth(),
-        3: IntrinsicColumnWidth(),
-      },
-      defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-      children: [
-        TableRow(
-          children: [
-            Text(
-              'Date',
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              'Sys',
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              'Dia',
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              'Pulse',
-              textAlign: TextAlign.center,
-            ),
-          ],
-        ),
-        TableRow(
-
-          children: [
-            Text(
-              'Date',
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              'Sys',
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              'Dia',
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              'Pulse',
-              textAlign: TextAlign.center,
-            ),
-          ],
-        ),
+  Widget build(BuildContext context) {
+    return DataTable(
+      columns: [
+        DataColumn(label: Text('Date')),
+        DataColumn(label: Text('Systolic')),
+        DataColumn(label: Text('Diastolic')),
+        DataColumn(label: Text('Pulse')),
       ],
+      rows:
+          listOfColumns // Loops through dataColumnText, each iteration assigning the value to element
+              .map(
+                ((element) => DataRow(
+                      cells: <DataCell>[
+                        DataCell(Text(element["Date"])), //Extracting from Map element the value
+                        DataCell(Text(element["Systolic"])),
+                        DataCell(Text(element["Diastolic"])),
+                        DataCell(Text(element["Pulse"])),
+                      ],
+                    )),
+              )
+              .toList(),
     );
   }
 }
