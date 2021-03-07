f3a826a67aa44f9262b002185c7d25a6d54307c5
Ahmad Afifi <ahmedafifi@Ahmads-MacBook-Pro.local>
2021-03-06 19:53:10 -0500

    Fixing Omar's shit

--- healthe/lib/ThirdPage.dart
+++ healthe/lib/ThirdPage.dart
@@ -4,11 +4,14 @@ import 'dart:math';
 import 'package:firebase_database/firebase_database.dart';
 
 class Third extends StatelessWidget {
+  void _incrementCounter() {
+    DatabaseReference _testRef =
+        FirebaseDatabase.instance.reference().child("test");
+    _testRef.set("Hello World ${Random().nextInt(100)}");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new Container(
-        child: new Center(
-            child:
-                new Icon(Icons.local_pizza, size: 150.0, color: Colors.teal)));
+    return FloatingActionButton(onPressed: _incrementCounter);
   }
 }
